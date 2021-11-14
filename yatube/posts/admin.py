7977880d840862f271e9ecf89c3cb411cfd14cbf
Edward <kt215m@gmail.com>
2021-11-14 15:06:03 +0300

    Added in admin.py attributes in admin classes

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -13,12 +13,19 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['text', 'pub_date', 'author', 'group']
+    list_display = ['pk', 'text', 'pub_date', 'author', 'group']
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    list_editable = ('group',)
+    empty_value_display = '-пусто-'
 
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'description']
+    search_fields = ('title', 'description')
+    list_filter = ('title',)
+    empty_value_display = '-пусто-'
 
 # admin.site.register(Post, PostAdmin)
 # admin.site.register(Group)
